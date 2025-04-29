5e281db244fd81718f4f1255f6aea1b58d73ea42
Jadwyn <jadwyn.tauhara@gmail.com>
2025-04-30 10:25:36 +1200

    age confirmation

--- camp.py
+++ camp.py
@@ -7,3 +7,17 @@ while fn == "":
     #check for valid input, if good then break the loop
     if fn != "": break
     else: fn = input("What is your first name? ")
+fn.lower()
+
+#asking and testing if age is 5-17
+try:
+    Age = int(input("Please enter your age: "))
+    if Age < 5:
+        print("Sorry, you are too young.")
+        exit()
+    elif Age > 17:
+        print("Sorry, you are too old.")
+        exit()
+# if there is an exception, print this out
+except ValueError:
+  print("That's not a valid number!")
