738ac8180180a9b8444ad6c34dfb3fc058c1bc87
Jadwyn <jadwyn.tauhara@gmail.com>
2025-04-30 10:20:50 +1200

    name test

---
+++ camp.py
@@ -0,0 +1,9 @@
+print("welcome")
+# ------ gather name info and age
+#making a while loop to see if the user doesnt leave it blank
+fn = "" # -- first name = fn
+while fn == "":
+    fn = input("What is your first name? ")
+    #check for valid input, if good then break the loop
+    if fn != " ": break
+    else: fn = ""
