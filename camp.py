2f0a690be1a99144ef3323fd477544243fe71fb1
Jadwyn <jadwyn.tauhara@gmail.com>
2025-04-30 10:28:56 +1200

    created the camp list

--- camp.py
+++ camp.py
@@ -1,15 +1,15 @@
 print("welcome")
 # ------ gather name info and age
-#making a while loop to see if the user doesnt leave it blank
+# making a while loop to see if the user doesnt leave it blank
 fn = "" # -- first name = fn
 while fn == "":
     fn = input("What is your first name? ")
-    #check for valid input, if good then break the loop
+    # check for valid input, if good then break the loop
     if fn != "": break
     else: fn = input("What is your first name? ")
 fn.lower()
 
-#asking and testing if age is 5-17
+# asking and testing if age is 5-17
 try:
     Age = int(input("Please enter your age: "))
     if Age < 5:
@@ -21,3 +21,11 @@ try:
 # if there is an exception, print this out
 except ValueError:
   print("That's not a valid number!")
+
+# -- informing the user
+
+print(f"welcome {fn}.")
+print("you will now be given a choice of Camps")
+
+# --- making the lists
+campList = ["Cultural immersion","Kayaking and pancakes","Mountain biking"]
