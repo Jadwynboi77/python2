85bfb49439b53ea0c8ef83d41f7f73855a66739c
Jadwyn <jadwyn.tauhara@gmail.com>
2025-04-30 10:22:11 +1200

    fixed name test

--- camp.py
+++ camp.py
@@ -5,5 +5,5 @@ fn = "" # -- first name = fn
 while fn == "":
     fn = input("What is your first name? ")
     #check for valid input, if good then break the loop
-    if fn != " ": break
-    else: fn = ""
+    if fn != "": break
+    else: fn = input("What is your first name? ")
