c569cd9845cfa6415bfb05f67518371675a4986b
Jadwyn <jadwyn.tauhara@gmail.com>
2025-05-01 11:52:52 +1200

    defined the list

--- camp.py
+++ camp.py
@@ -29,6 +29,20 @@ print("you will now be given a choice of Camps")
 
 # --- making the lists
 campList = ["Cultural immersion","Kayaking and pancakes","Mountain biking"]
+mealList = ["standered","vegetarain","dairy-free"]
 dayList = [5,3,4]
 costList = [800,400,900]
 dificultyList = ["easy","moderite","difficult"]
+count = 0
+#the bypass allows this list to start at 1, instead of starting at 0
+
+#creating the defines (tuples)
+def Camp_list():
+    print(f'1. {campList[count]}, included with a fee of {costList[count]} and is considered "{dificultyList[count]}".')
+    print(f'2. {campList[count + 1]}, included with a fee of {costList[count + 1]} and is considered "{dificultyList[count + 1]}".')
+    print(f'3. {campList[count + 2]}, included with a fee of {costList[count + 2]} and is considered "{dificultyList[count + 2]}".')
+
+def meal_list():
+    print(f'1. {mealList[count]}')
+    print(f'2. {mealList[count + 1]}')
+    print(f'3. {mealList[count + 1]}')
